5bd861f1e1a9d3c5212ec8c994ea8595dd9e6f3d
javier2002limon <javier.limon@alumnos.upm.es>
2025-10-02 21:13:17 +0200

    Primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useLenis } from "./hooks/useLenis";
 import Hero from "./components/Hero";
 import ComoFunciona from "./components/ComoFunciona";
-import AsignaturasMetodologia from "./components/AsignaturasMetodologia";
 import Seccion3 from "./components/Seccion3";
 import Seccion4 from "./components/Seccion4";
 
